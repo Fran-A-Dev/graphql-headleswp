682cdcaf0f231a1e25b89d01c163017774c76784
Francis Agulto <francisagulto@franciss-MBP.attlocal.net>
2021-12-28 09:30:35 -0600

    scaffolded app wrapper

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import {
+    ApolloClient,
+    ApolloProvider,
+    HttpLink,
+    InMemoryCache,
+} from "@apollo/client";
+
+import { useMemo } from 'react';
+
+
+function createApolloClient() {
+    return new ApolloClient({
+        ssrMode: typeof window === "undefined",
+        link: new HttpLink({
+            uri: "http://headless-gql-demo.local/graphql",
+        }),
+        cache: new InMemoryCache(),
+    })
+}
+
+export default function App ({ Component , pageProps }) {
+    const apolloClient = useMemo(() => createApolloClient(), []);
+
+    return (
+        <ApolloProvider client={apolloClient}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    )
+
+
+}
