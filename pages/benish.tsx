a6f15ca8b622e315435e4b1efc7c7942e483f5b3
Francis Agulto <francisagulto@franciss-MBP.attlocal.net>
2022-02-08 16:44:29 -0600

    added new feature and page

---
+++ pages/benish.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div>
+      <h1>Benish Blog</h1>
+      <p>This is my first blog on headless archtiecture</p>
+    </div>
+  );
+}
